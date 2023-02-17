a747e6850f2c2cd84a9f13ca2931b6b6c5f014e0
yoonisali <yoonismahamoudali@gmail.com>
2023-02-17 11:25:23 -0800

    add horizontal and vertical axis to board using two for loops to display the arrays, will now start tiles

--- src/components/Chessboard.jsx
+++ src/components/Chessboard.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 
+const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"]
+const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"]
+
 function Chessboard() {
+  let board = [];
+  for(let j = verticalAxis.length - 1; j >= 0; j--) {
+    for(let i = 0; i < horizontalAxis.length; i++) {
+      board.push (
+      <div className="tile">[{horizontalAxis[i]} {verticalAxis[j]}]</div>
+      )
+    }
+  }
   return (
-    <div className="chessboard"></div>
+    <div id="chessboard">{board}</div>
   )
 }
 
