93ccfb56ddc543204f195dd7ac4b048f31469469
yoonisali <yoonismahamoudali@gmail.com>
2023-02-17 09:06:28 -0800

    add images for white and black pieces into the images folder, also create Chessboard component

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import Chessboard from './Chessboard';
+
+function App() {
+  return (
+    <div>
+      <Chessboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Chessboard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Chessboard() {
+  return (
+    <div>I am the chessboard lol</div>
+  )
+}
+
+export default Chessboard;
