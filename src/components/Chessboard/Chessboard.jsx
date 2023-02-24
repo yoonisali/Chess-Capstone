5ddaa0dd610e99c8032f125eae365973c8985de3
yoonisali <yoonismahamoudali@gmail.com>
2023-02-24 11:44:58 -0800

    pieces are now loaded in dynamically and image is passed down as a prop

--- src/components/Chessboard/Chessboard.jsx
+++ src/components/Chessboard/Chessboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Tile from "../Tile/Tile";
+import Tile  from "../Tile/Tile";
 
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"]
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"]
@@ -10,7 +10,10 @@ function Chessboard() {
     for (let i = 0; i < horizontalAxis.length; i++) {
         const number = j + i + 2;
 
-        board.push(<Tile number={number} />)
+        board.push(<Tile 
+          number={number} 
+          image="./assets/img/bPawn.png"  
+          />)
 
         // if (number % 2 === 0) {
         //   board.push(
--- src/components/Tile/Tile.jsx
+++ src/components/Tile/Tile.jsx
@@ -3,14 +3,15 @@ import PropTypes from "prop-types"
 
 function Tile(props) {
   if (props.number % 2 === 0) {
-    return <div className="tile green-tile"><img src="./assets/img/bPawn.png"/></div>
+    return <div className="tile green-tile"><img src={props.image}/></div>
   } else {
-    return <div className="tile white-tile"><img src="./assets/img/wPawn.png"/></div>
+    return <div className="tile white-tile"><img src={props.image}/></div>
   }
 }
 
 Tile.propTypes = {
-  number: PropTypes.number
+  number: PropTypes.number,
+  image: PropTypes.string
 }
 
 export default Tile;
