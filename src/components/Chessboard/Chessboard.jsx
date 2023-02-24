e2d50f89816ecded95f65fbabb3f4158c1735cc1
yoonisali <yoonismahamoudali@gmail.com>
2023-02-24 13:10:33 -0800

    add first black pawn to be rendered on it's correct postion

--- src/components/Chessboard/Chessboard.jsx
+++ src/components/Chessboard/Chessboard.jsx
@@ -1,35 +1,34 @@
 import React from "react";
-import Tile  from "../Tile/Tile";
+import Tile from "../Tile/Tile";
 
-const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"]
-const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"]
+const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
+const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+const pieces = [];
+
+pieces.push({ image: "./assets/img/bPawn.png", x: 0, y: 6 });
 
 function Chessboard() {
   let board = [];
+
   for (let j = verticalAxis.length - 1; j >= 0; j--) {
     for (let i = 0; i < horizontalAxis.length; i++) {
-        const number = j + i + 2;
+      const number = j + i + 2;
+      let image = undefined;
 
-        board.push(<Tile 
-          number={number} 
-          image="./assets/img/bPawn.png"  
-          />)
+      pieces.forEach((p) => {
+        if (p.x === i && p.y === j) {
+          image = p.image;
+        }
+      });
 
-        // if (number % 2 === 0) {
-        //   board.push(
-        //     <div className="tile green-tile">[{horizontalAxis[i]}{verticalAxis[j]}]</div>
-        //   )
-        // } else {
-        //   board.push(
-        //     <div className="tile white-tile">[{horizontalAxis[i]}{verticalAxis[j]}]</div>
-        //   )
-        // }
-      }
+      board.push(<Tile 
+        number={number} 
+        image={image} />);
     }
-      return (
-        <div id="chessboard">{board}</div>
-      )
-    
   }
+
+  return <div id="chessboard">{board}</div>;
+}
 
 export default Chessboard;
