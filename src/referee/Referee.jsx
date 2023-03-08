093b36a5c3e9be5048957c4a7a8f71965abef09c
yoonisali <yoonismahamoudali@gmail.com>
2023-03-08 09:22:58 -0800

    add black pawn movement and WIP adding whether tile is occupied method

--- src/referee/Referee.jsx
+++ src/referee/Referee.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-import { PieceType } from "../components/Chessboard/Chessboard";
-import { TeamType } from "../components/Chessboard/Chessboard";
+import { PieceType, TeamType } from "../components/Chessboard/Chessboard";
+
 
 export default class Referee {
+
+  tileIsOccupied(x, y, boardState) {
+
+  }
+
+
   isValidMove(px, py, x, y, type, team) {
     console.log(`Previous Location: (${px}, ${py})`)
     console.log(`Current Location: (${x}, ${y})`)
@@ -18,6 +24,16 @@ export default class Referee {
       
       } else {
         if(px === x && y - py === 1) {
+          return true;
+        }
+      }
+    } else {
+      if (py === 6) {
+        if (px === x && (y - py === -1 || y - py === -2)) {
+          return true;
+        }
+      } else {
+        if(px === x && y - py === -1) {
           return true;
         }
       }
